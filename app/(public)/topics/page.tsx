822de316a9db8f3e843e0365dab22cecd3d6467f
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2026-03-21 20:46:27 +0200

    Add Topics page and Topics_Card component to display topics

--- app/(public)/topics/page.tsx
+++ app/(public)/topics/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { topics } from '@/data/topic'
+import Topics_Card from '@/components/Topics_Card'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <h1>Topics</h1>
+    <p>Explore the most popular topics in the developer community. Find discussions and posts that interest you</p>
+    <div>
+      <ul>
+        {topics.map((topic)=>(
+          <li key={topic}>
+            <Topics_Card topic={topic} />
+          </li>
+        ))}
+      </ul>
+    </div>
+    </>
   )
 }
 
