e16929eca5e64a82519ef0f582227e57225f749a
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2026-03-21 15:32:58 +0200

    Added Navbar and Footer components; update RootLayout to include them; added a loading page at Root

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* <p>Built for developers, by developers</p> */}
+      <p>&copy; {new Date().getFullYear()} Dev Community. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
