1e114a772f3c2a9c9ffed634946716c615b64993
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2026-03-21 16:30:01 +0200

    added home and about pages

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <h1>About DevCommunity</h1>
+      <p>DevCommunity was built for developers by developers. Our mission is to create a space where programmers of all levels can share ideas, ask questions, and build meaningful connections. Whether you are just starting out or a seasoned engineer, there is a place for you here.</p>
+    </>
   )
 }
 
