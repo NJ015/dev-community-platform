f26e48c9fff923328092c8cb1297b795277e28bc
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2026-03-22 17:51:29 +0200

    Refactored root layout and styling for improved UI consistency; updated Navbar and Footer components with new styles and metadata

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,70 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  return (
-    <nav className='flex gap-6'>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/communities"}>Communities</Link>
-        <Link href={"/developers"}>Developers</Link>
-        <Link href={"/topics"}>Topics</Link>
-    </nav>
-  )
-}
+  const pathname = usePathname();
 
-export default Navbar
+  return (
+    <nav className="flex bg-gray-800 px-8 py-4 items-center justify-between border-b border-gray-700">
+      <Link href={"/"} className="text-white font-bold text-xl">
+        DevCommunity
+      </Link>
+      <div className="flex gap-6 items-center">
+      <Link
+        href={"/"}
+        className={
+          pathname === "/"
+            ? "text-white font-bold"
+            : "text-gray-400 hover:text-white transition-colors duration-300"
+        }
+      >
+        Home
+      </Link>
+      <Link
+        href={"/about"}
+        className={
+          pathname === "/about"
+            ? "text-white font-bold"
+            : "text-gray-400 hover:text-white transition-colors duration-300"
+        }
+      >
+        About
+      </Link>
+      <Link
+        href={"/communities"}
+        className={
+          pathname === "/communities"
+            ? "text-white font-bold"
+            : "text-gray-400 hover:text-white transition-colors duration-300"
+        }
+      >
+        Communities
+      </Link>
+      <Link
+        href={"/developers"}
+        className={
+          pathname === "/developers"
+            ? "text-white font-bold"
+            : "text-gray-400 hover:text-white transition-colors duration-300"
+        }
+      >
+        Developers
+      </Link>
+      <Link
+        href={"/topics"}
+        className={
+          pathname === "/topics"
+            ? "text-white font-bold"
+            : "text-gray-400 hover:text-white transition-colors duration-300"
+        }
+      >
+        Topics
+      </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
